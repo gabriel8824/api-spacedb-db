79f4751aa1f0887fd7f3f8262edaf8dfb195fd02
Gabriel Lenon <gabriellenongb@gmail.com>
2023-06-15 10:45:31 -0300

    correçao cors api

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const routes = require('./src/routes');
 
+// Configurar as opções do CORS
+const corsOptions = {
+  origin: 'https://spacedb-3qwus3.flutterflow.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Aplicar o middleware do CORS
+app.use(cors(corsOptions));
+
 const app = express();
 app.use(express.json());
 
