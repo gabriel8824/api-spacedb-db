8bfb6f494d6cbad83be0f0eee4e40c4bfeb07ae9
Gabriel Lenon <gabriellenongb@gmail.com>
2023-06-15 08:38:00 -0300

    Commit endpoint listar colunas

---
+++ src/controllers/listColumnsController.js
@@ -0,0 +1,31 @@
+const { Sequelize } = require('sequelize');
+
+async function listColumns(req, res) {
+  try {
+    const { host, port, username, password, database, dialect } = req.body;
+    const { tableName } = req.params;
+
+    const sequelize = new Sequelize(database, username, password, {
+      host,
+      port,
+      dialect,
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    });
+
+    // Obter as colunas da tabela
+    const columns = await sequelize.queryInterface.describeTable(tableName);
+
+    res.json({ success: true, columns });
+  } catch (error) {
+    console.error('Erro ao obter as colunas da tabela:', error);
+    res.status(500).json({ success: false, error: 'Erro ao obter as colunas da tabela' });
+  }
+}
+
+module.exports = listColumns;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const createTableController = require('../controllers/createTableController');
 const deleteTableController = require('../controllers/deleteTableController');
 const listTablesController = require('../controllers/listTablesController');
 const renameTableController = require('../controllers/renameTableController');
+const listColumnsController = require('../controllers/listColumnsController');
 
 router.post('/verificar-conexao', verifyConnectionController);
 router.post('/criar-tabela', createTableController);
 router.post('/deletar-tabela/:tableName', deleteTableController);
 router.post('/listar-tabelas', listTablesController);
 router.post('/renomear-tabela/:oldTableName/:newTableName', renameTableController);
+router.post('/listar-colunas/:tableName', listColumnsController);
 
 module.exports = router;
