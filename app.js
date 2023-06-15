2a43c6976f6e4932df1c55cf8e83d655c190ae3d
Gabriel Lenon <gabriellenongb@gmail.com>
2023-06-15 10:48:29 -0300

    correçao cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./src/routes');
+const cors = require('cors');
 
 // Configurar as opções do CORS
 const corsOptions = {
